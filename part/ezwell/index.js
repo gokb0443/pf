acc999c6ecbdb5ab7e2dc1b035fa96ee06acabf1
gokb0443 <wonjinssi2@naver.com>
2020-07-28 20:27:50 +0900

    2020 07 28 작업

---
+++ part/ezwell/index.js
@@ -0,0 +1,11 @@
+console.clear();
+
+
+$('.top-bar .menu-box-1').mouseenter(function() {
+  $('.top-bar').addClass('hover');
+});
+
+
+$('.top-bar .menu-box-1').mouseleave(function() {
+  $('.top-bar').removeClass('hover');
+});
