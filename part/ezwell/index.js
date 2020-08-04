c25b24948e0c6a653d9f1f345c89f6fb5b7f6c82
unknown <wonjinssi2@naver.com>
2020-08-04 21:29:19 +0900

    2020 08 04 작업

--- part/ezwell/index.js
+++ part/ezwell/index.js
@@ -1,6 +1,5 @@
 console.clear();
 
-
 $('.top-bar .menu-box-1').mouseenter(function() {
   $('.top-bar').addClass('hover');
 });
@@ -8,4 +7,13 @@ $('.top-bar .menu-box-1').mouseenter(function() {
 
 $('.top-bar .menu-box-1').mouseleave(function() {
   $('.top-bar').removeClass('hover');
+});
+
+$('.top-bar .menu-box-1').mouseenter(function() {
+  $('.top-bar>.menu-box-1>.bg-box').addClass('active');
+});
+
+
+$('.top-bar .menu-box-1').mouseleave(function() {
+  $('.top-bar>.menu-box-1>.bg-box').removeClass('active');
 });
